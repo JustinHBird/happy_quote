d98f8b2fc4ba81b967c946c65f76580e3288510d
Justin Bird <justin.h.bird@gmail.com>
2020-03-06 18:02:49 -0800

    Landing page registration layout. Basic positioning

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
 
 # Flask Plugins
-from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
 # Load Config
 app.config.from_object(Config)
 
-# Initialize Plugins
-bootstrap = Bootstrap(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,19 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from app import app
 from app.forms import LoginForm, RegisterForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    #if request.method == 'GET':
+
+    form = RegisterForm()
+    return render_template('index.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    
     form = LoginForm()
     return render_template('login.html', title='Log In', form=form)
 
