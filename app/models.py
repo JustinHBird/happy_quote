f1d1bb037d7ec548232cf4d231ced956a32a9d37
Justin Bird <justin.h.bird@gmail.com>
2020-03-09 16:38:39 -0700

    Changed database 'Quotes' table to 'Quote'

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.email}>'
 
-class Quotes(db.Model):
+class Quote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     quote = db.Column(db.String(240))
